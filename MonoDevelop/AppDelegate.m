8e8847f8132c45cf0874d6bd94c714f11e4eb0da
GaoLei <dongxie@dongxie.cn>
2012-12-26 19:38:40 +0800

    Initial Commit

---
+++ MonoDevelop/AppDelegate.m
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.m
+//  MonoDevelop
+//
+//  Created by gaolei on 12-12-26.
+//  Copyright (c) 2012年 gaolei. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ MonoDevelop/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  MonoDevelop
+//
+//  Created by gaolei on 12-12-26.
+//  Copyright (c) 2012年 gaolei. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
