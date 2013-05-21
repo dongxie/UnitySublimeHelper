390add5713dba387a2b2d531ff23815d435a37e0
Gao Lei <gaolei@GaotekiMacBook-Pro.local>
2013-05-21 11:24:59 +0800

    correct upload iszzue

--- MonoDevelop/AppDelegate.h
+++ MonoDevelop/AppDelegate.h
@@ -1,7 +1,7 @@
 //
 //  AppDelegate.h
-//  MonoDevelop
-//
+//  UnitySublimeHelper
+//  
 //  Created by gaolei on 12-12-26.
 //  Copyright (c) 2012年 gaolei. All rights reserved.
 //
@@ -13,3 +13,12 @@
 @property (assign) IBOutlet NSWindow *window;
 
 @end
+struct SelectionRange
+{
+	short   unused1;	// 0 (not used)
+	short   lineNum;	// line to select (< 0 to specify range)
+	long	startRange; // start of selection range (if line < 0)
+	long	endRange;   // end of selection range (if line < 0)
+	long	unused2;	// 0 (not used)
+	long	theDate;	// modification date/time
+};
--- MonoDevelop/AppDelegate.m
+++ MonoDevelop/AppDelegate.m
@@ -7,7 +7,8 @@
 //
 
 #import "AppDelegate.h"
-
+//#import <Carbon/Carbon.h>
+static NSString* TextMateBundleIdentifier = @"com.happiplay.MonoDevelop";
 @implementation AppDelegate
 
 - (void)dealloc
@@ -18,6 +19,42 @@
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
     // Insert code here to initialize your application
+    
+    NSAppleEventDescriptor*  evt = [[NSAppleEventManager sharedAppleEventManager] currentAppleEvent];
+	if( evt )
+	{
+		NSAppleEventDescriptor*  param = [evt paramDescriptorForKeyword: keyAEPosition];
+		if( param )		// This is always false when xCode calls us???
+		{
+			NSData*					data = [param data];
+            NSString* desc=[data description];
+		}
+	}
 }
-
+-(void)application:(NSApplication *)sender openFiles:(NSArray *)filenames{
+  
+    NSString* filename=[filenames objectAtIndex:0];
+    if([filename rangeOfString:@".cs"].location!=NSNotFound||[filename rangeOfString:@".txt"].location!=NSNotFound||[filename rangeOfString:@".js"].location!=NSNotFound||[filename rangeOfString:@".rsp"].location!=NSNotFound){
+        NSAppleEventDescriptor*  evt = [[NSAppleEventManager sharedAppleEventManager] currentAppleEvent];
+        if( evt )
+        {
+            NSAppleEventDescriptor*  param = [evt paramDescriptorForKeyword: keyAEPosition];
+            if( param )		// This is always false when xCode calls us???
+            {
+                NSData*					data = [param data];
+                
+                struct SelectionRange   range;
+                
+                memmove( &range, [data bytes], sizeof(range) );
+                NSString* execstr=[NSString stringWithFormat:@"\"/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl\" \"%@:%d\"",filename,range.lineNum+1];
+                NSLog(@"%@",execstr);
+                system([execstr UTF8String]);
+                exit(0);
+            }
+        }
+    }
+    
+    
+    
+}
 @end
